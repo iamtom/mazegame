6aed290b27abc2ccc704611a81a92290f6062eaa
iamtom <tomd.470@gmail.com>
2014-03-29 22:13:47 +0000

    Game now gives the player their time taken to complete the level.
    Now have level selection buttons.
    
    Detects when the player enters the level and reaches the finish.

--- mazegame.js
+++ mazegame.js
@@ -3,22 +3,18 @@ $(document).ready(function() {
 	canvas.width = 1000;
 	canvas.height = 500;
 
-	oneUnit = 25;
-
-	//Should focus mouse to canvas here and place it in the start position
-
-	//If mouse leaves canvas remember its last coordinates. When mouse re-enters move it to those co-ordinates
+	var oneUnit = canvas.height/20; //25
 
 	//Level completition will be timed. Should start from when the mouse leaves the green until it enters the red.
 
 	//If the player touches the wall they die. This stops them just rushing through and bouncing off the walls to get the best score
 
 	//HARD MODE: A black screen covers the maze. There is a circle of visibility surrounding the players mouse. This is another canvas on top of the bottom one. The circle is completely see-through
-
-	var pX = 0, pY =0;
-	var logOfX = [];
-	var logOfY = [];
+	
 	var score = 0;
+	var timerOn = false;
+	var startTime;
+	var endTime;
 	var floorColour;
 	var wallColour;
 	var startColour;
@@ -30,6 +26,10 @@ $(document).ready(function() {
 	context.lineWidth = 1;
 	context.strokeStyle = 'black';
 
+	function resetThings() {
+
+	}
+
 	//Mouse movement
 	function getMousePosition(canvas, event) {
 		var rect = canvas.getBoundingClientRect();
@@ -38,65 +38,38 @@ $(document).ready(function() {
 			y: event.clientY - rect.top
 		};
 	}	
-	//Drawing with the mouse
-	$('#canvas').mouseenter(function(event) {
-		pos = getMousePosition(canvas, event);
-		posX = pos.x;
-		posY = pos.y;
-		
-		context.beginPath();
-		logOfX.push(posX);
-		logOfY.push(posY);
-		$('#canvas').mousemove(function(event) {
-			pos = getMousePosition(canvas, event);
-			posX = pos.x;
-			posY = pos.y;
-			/* //Just so I can see the co-ordinates in the console:
-			var msg = "Mouse called at ";
-			msg += event.pageX + ", " + event.pageY;
-			console.log(msg); */
 
-			collisionDetect(posX, posY);
-
-			//Draw the lines:
-			context.moveTo(logOfX[logOfX.length], logOfY[logOfY.length]);
-			context.lineTo(posX, posY);
-			context.stroke();			
-
-			if (logOfX.length && logOfY.length > 10) {
-				logOfX = [];
-				logOfY = [];
-			}
-		
-		});
-	});
 	//Collision
 	function collisionDetect(playerX, playerY) {
-		var collide = false;
+		var checkColour;
 		playerBox = context.getImageData(playerX, playerY, 1, 1);
+
 		// console.log(playerBox);
 		// console.log(wallColour);
 		for (i = 0; i < playerBox.data.length; i+4){
-			var checkColour = 'rgb(' + playerBox.data[i] + ',' + playerBox.data[i+1] + ',' + playerBox.data[i+2] + ')';
-			if (checkColour == wallColour) {
-				console.log('you died!');
-				//display game over screen
-				gameOver();
-				//game over function here()
-
-			} else if (checkColour == endColour) {
-				console.log('you win!');
-				//display win screen
-				levelComplete();
-				//level complete function here()
-			}
+			checkColour = 'rgb(' + playerBox.data[i] + ',' + playerBox.data[i+1] + ',' + playerBox.data[i+2] + ')';
 			break;
+		}
+		if (checkColour == wallColour) {
+			console.log('you died!');
+			//display game over screen
+			timerStop();				
+			gameOver();
+		} else if (checkColour == endColour /*&& timerOn == true*/) {
+			//display win screen and stop timer
+			timerStop();
+			levelComplete();
+		} else if (checkColour == startColour /*&& timerOn == false*/) {
+			//Start the timer
+			timerStart();
 		}
 	}
 
 
 	//Level construction
 	function drawMaze(level, levelData) {
+		pX = 0; 
+		pY =0;
 		//draw the maze based on what level is called. 0 = white/empty, 1 = grey/wall, 2 = green/start, 3 = red/end
 		floorColour = 'rgb(' + levelData.floor[0] + ',' + levelData.floor[1] + ',' + levelData.floor[2] + ')';
 		wallColour = 'rgb(' + levelData.wall[0] + ',' + levelData.wall[1] + ',' + levelData.wall[2] + ')';
@@ -107,7 +80,8 @@ $(document).ready(function() {
 			//Go through each row in the array
 			for (j = 0; j < level[i].length; j++) {
 				//Go through the row and draw by numbers
-				drawBlock(level[i][j]);
+				typeOfBlock = level[i][j]
+				drawBlock(typeOfBlock);
 				pX += oneUnit;
 			}
 			pX = 0;
@@ -140,34 +114,95 @@ $(document).ready(function() {
 		context.fillRect(pX, pY, oneUnit, oneUnit);
 	}
 
-
-	//Game
-	$('#start').click(function() {
-		$('#startScreen').css('visibility', 'hidden');
-		drawMaze(level1, level1Data);
-		//put mouse at specified start co-ords
-		//Start timer
-		//Button to next level
-		$('#start').css('visibility', 'hidden');
-	});
-
-	function gameOver() {
-		//End timer
-		//Show game over screen
-		$('#gameOverScreen').css('visibility', 'visible');
-		//restart button	
+	//Timer
+	function timerStart() {
+		//Start the timer
+		timerOn = true;
+		startTime = new Date();
+	}
+	function timerStop() {
+		//stop timer
+		//score = time
+		//write score to object containing player times for each level
+		timerOn = false;
+		endTime = new Date();	
 	}
 
+	function startLevel(level, levelData) {
+		//load level and appropriate level data
+		$('#startScreen').css('visibility', 'hidden');
+		$('#levelCompleteScreen').css('visibility', 'hidden');
+		$('#gameOverScreen').css('visibility', 'hidden');
+		$('#levelSelection').css('visibility', 'hidden');
+		drawMaze(level, levelData);
+	}
 	function levelComplete() {
 		//End timer
 		//show level complete screen
 		$('#levelCompleteScreen').css('visibility', 'visible');
 		//display time/score
-		//change next level
-		//button to start next level
+		$('#levelSelection').css('visibility', 'visible');
+		var playerTime = (endTime.getTime() - startTime.getTime())/1000;
+		$('#levelCompleteScreen').html('<h1>Winner!</h1><h2>Your time: ' + playerTime + ' seconds</h2>');
+	}		
+	function gameOver() {
+		//End timer
+		//Show game over screen
+		$('#gameOverScreen').css('visibility', 'visible');
+		$('#levelSelection').css('visibility', 'visible');
 	}
+
+	
+
+
+	//Level selection buttons
+	$('#startLevel1').click(function() {
+		startLevel(map1, map1Data);
+	});
+	$('#startLevel2').click(function() {
+		startLevel(map2, map2Data);
+	});
+	$('#startLevel3').click(function() {
+		startLevel(map3, map3Data);
+	});
+
+	//Drawing with the mouse
+	$('#canvas').mouseenter(function(event) {
+		var logOfX = [];
+		var logOfY = [];
+		pos = getMousePosition(canvas, event);
+		posX = pos.x;
+		posY = pos.y;
+				
+		context.beginPath();
+		logOfX.push(posX);
+		logOfY.push(posY);
+		$('#canvas').mousemove(function(event) {
+			pos = getMousePosition(canvas, event);
+			posX = pos.x;
+			posY = pos.y;
+			/* //Just so I can see the co-ordinates in the console:
+			var msg = "Mouse called at ";
+			msg += event.pageX + ", " + event.pageY;
+			console.log(msg); */
+
+			collisionDetect(posX, posY);
+
+			//Draw the lines:
+			context.moveTo(logOfX[logOfX.length], logOfY[logOfY.length]);
+			context.lineTo(posX, posY);
+			context.stroke();			
+
+			if (logOfX.length && logOfY.length > 10) {
+				logOfX = [];
+				logOfY = [];
+			}
 		
-	drawMaze(level1, level1Data);
+		});
+	});
+
+
+	
 	
 
 });
