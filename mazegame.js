435a2a83b3208008dc73338b365a46ab248fa7d6
iamtom <tomd.470@gmail.com>
2014-03-30 17:37:50 +0100

    Upon winning or losing a level the mousemove is now unbound. This stops the game from lagging like it used to after being played a while without refreshing the page.

--- mazegame.js
+++ mazegame.js
@@ -5,12 +5,6 @@ $(document).ready(function() {
 
 	var oneUnit = canvas.height/20; //25
 
-	//Level completition will be timed. Should start from when the mouse leaves the green until it enters the red.
-
-	//If the player touches the wall they die. This stops them just rushing through and bouncing off the walls to get the best score
-
-	//HARD MODE: A black screen covers the maze. There is a circle of visibility surrounding the players mouse. This is another canvas on top of the bottom one. The circle is completely see-through
-	
 	var score = 0;
 	var timerOn = false;
 	var startTime;
@@ -24,13 +18,14 @@ $(document).ready(function() {
 	context.lineCap = 'round';
 	context.lineJoin = 'round';
 	context.lineWidth = 1;
-	context.strokeStyle = 'black';
+	context.strokeStyle = 'black';	
 
-	function resetThings() {
+	//Level completition will be timed. Should start from when the mouse leaves the green until it enters the red.
 
-	}
+	//If the player touches the wall they die. This stops them just rushing through and bouncing off the walls to get the best score
 
-	//Mouse movement
+	//HARD MODE: A black screen covers the maze. There is a circle of visibility surrounding the players mouse. This is another canvas on top of the bottom one. The circle is completely see-through
+	//Mouse
 	function getMousePosition(canvas, event) {
 		var rect = canvas.getBoundingClientRect();
 		return {
@@ -38,6 +33,11 @@ $(document).ready(function() {
 			y: event.clientY - rect.top
 		};
 	}	
+	function unbindMouse() {
+		$('#canvas').unbind('mousemove');
+		logOfX = [];
+		logOfY = [];
+	}
 
 	//Collision
 	function collisionDetect(playerX, playerY) {
@@ -139,6 +139,7 @@ $(document).ready(function() {
 	function levelComplete() {
 		//End timer
 		//show level complete screen
+		unbindMouse();
 		$('#levelCompleteScreen').css('visibility', 'visible');
 		//display time/score
 		$('#levelSelection').css('visibility', 'visible');
@@ -148,10 +149,10 @@ $(document).ready(function() {
 	function gameOver() {
 		//End timer
 		//Show game over screen
+		unbindMouse();
 		$('#gameOverScreen').css('visibility', 'visible');
 		$('#levelSelection').css('visibility', 'visible');
 	}
-
 	
 
 
